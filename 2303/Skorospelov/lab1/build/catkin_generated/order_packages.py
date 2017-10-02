d6e8ba77c07a16e844f4324022f21a722f4c69d5
LeoSko <leosko94@mail.ru>
2017-10-02 03:18:01 +0300

    Delete trash

--- 2303/Skorospelov/lab1/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/leosko/ROS/ros_2017/2303/Skorospelov/lab1/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/leosko/ROS/ros_2017/2303/Skorospelov/lab1/devel;/opt/ros/kinetic".split(';') if "/home/leosko/ROS/ros_2017/2303/Skorospelov/lab1/devel;/opt/ros/kinetic" != "" else []
--- 2303/Skorospelov/lab1/build/maze/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leosko/ROS/ros_2017/2303/Skorospelov/lab1/src/maze
-# Build directory: /home/leosko/ROS/ros_2017/2303/Skorospelov/lab1/build/maze
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
